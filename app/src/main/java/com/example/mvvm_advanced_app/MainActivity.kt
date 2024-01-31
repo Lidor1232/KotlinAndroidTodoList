6f34c7bef16b8640f061396040a59b6f61fadd9b
lidordvir <lidor4001@gmail.com>
2024-01-31 15:46:57 +0200

    First Commit

---
+++ app/src/main/java/com/example/mvvm_advanced_app/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mvvm_advanced_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.mvvm_advanced_app.src.TodoListApp
+import com.example.mvvm_advanced_app.ui.theme.Mvvm_advanced_appTheme
+import timber.log.Timber
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Timber.plant(
+            object : Timber.DebugTree() {
+                /**
+                 * Override [log] to modify the tag and add a "global tag" prefix to it. You can rename the String "global_tag_" as you see fit.
+                 */
+                override fun log(
+                    priority: Int,
+                    tag: String?,
+                    message: String,
+                    t: Throwable?,
+                ) {
+                    super.log(priority, "global_tag_$tag", message, t)
+                }
+            },
+        )
+
+        Timber.d("App created!")
+
+        setContent {
+            Mvvm_advanced_appTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    TodoListApp()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mvvm_advanced_app/src/TodoListApp.kt
@@ -0,0 +1,9 @@
+package com.example.mvvm_advanced_app.src
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TodoListApp() {
+    Text(text = "Hello World")
+}
